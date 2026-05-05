72cce3d06013b17702217200fa296823affdce91
63829 <63829@gymnasium.ax>
2026-05-05 17:00:14 +0300

    removed profileview from index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-import ProfileView from '@/views/ProfileView.vue'
+
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -18,11 +18,7 @@ const router = createRouter({
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue'),
     },
-    {
-      path: '/profile',
-      name: 'profile',
-      component: ProfileView,
-    },
+    
   ],
 })
 
